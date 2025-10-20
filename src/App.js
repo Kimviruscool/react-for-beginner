794f401d9212d1802bd54499f0c8a5e6ac8af2da
Kimviruscool <sinsa9122@naver.com>
2025-10-20 15:41:22 +0900

    React Chapter 5 Nomad code

--- src/App.js
+++ src/App.js
@@ -1,7 +1,13 @@
+import {useState} from  "react";
+
 function App() {
+  const [counter, setValue] = useState(0);
+  const onClick = () => setValue((prev) => prev + 1);
+  console.log("render")
   return (
     <div>
-      <h1>Welcome back!!!</h1>
+      <h1>{counter}</h1>
+      <button onClick={onClick}> click me</button>
     </div>
   );
 }
