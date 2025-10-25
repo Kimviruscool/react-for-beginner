3d4281a9eb3076859797718178f7dda56cd14177
Kimviruscool <sinsa9122@naver.com>
2025-10-25 14:28:43 +0900

    React 이론 끝

--- src/App.js
+++ src/App.js
@@ -1,46 +1,28 @@
 import {useState, useEffect, use} from  "react";
 
+function Hello() {
+
+  function ByFn(){
+    console.log("Bye")
+  }
+
+  function HiFn(){
+    console.log("created!")
+    return ByFn;
+  }
+  useEffect(HiFn, [])
+  return <h1>Hello</h1>
+}
+
 function App() {
-  const [counter, setValue] = useState(0);
+  const [showing, setShowing] = useState(false);
+  const onClick = () => setShowing((prev) => !prev);
 
-  const [keyword, setKeyword] = useState("")
-  
-  const onClick = () => setValue((prev) => prev + 1);
+  return <div>
+    {showing ? <Hello /> : null }
+    <button onClick={onClick} >{showing ? "Hide" : "Show"}</button>
+  </div>
 
-  const onChange = (event) => setKeyword(event.target.value);
-  
-  //상태가 변할때만 콘솔로그발생
-  useEffect(() => {
-    console.log("I run only once.");
-  }, []);
-  
-  //입력에 변화가있을때
-  useEffect(()=> {
-    //  if(keyword !== "" && keyword.length > 5){ 
-    //   //만약 value가 공백이아니거나 5보다 길면 콘솔로그
-    //     console.log("I run when 'keyword' changes.");
-    //  }
-    console.log("I run when 'keyword' changes.");
-  }, [keyword]);
-  //[]안에다가 value 입력
-  
-  //카운터에 변화가있을때
-  useEffect(() => {
-    console.log("I run when 'counter' chages.");
-  }, [counter])
-
-  //모두 변화가 있을때
-  useEffect(() => {
-    console.log("I run when keword & counter changers");
-  }, [keyword, counter]);
-
-  return (  
-    <div>
-      <input value={keyword} onChange={onChange} type="text" placeholder="Search Here " />
-      <h1>{counter}</h1>
-      <button onClick={onClick}> click me</button>
-    </div>
-  );
 }
 
 export default App;
